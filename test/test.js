cadf9a299592d2d99f41185dd9952daef6b76360
Joseph DeAngeloi <josephdeangeloi@Josephs-MBP.home>
2023-02-12 11:56:34 -0500

    update

--- test/test.js
+++ test/test.js
@@ -1,126 +1,81 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
-
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+const { assert } = require('chai');
+describe('MultiSigWallet', function () {
+    let contract;
+    let accounts;
+    beforeEach(async () => {
+        accounts = await ethers.provider.listAccounts();
+        const MultiSig = await ethers.getContractFactory("MultiSigWallet");
+        contract = await MultiSig.deploy(accounts.slice(0, 3), 1);
+        await contract.deployed();
     });
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    describe('storing ERC20 tokens', function () {
+        const initialBalance = 10000;
+        let token;
 
-      expect(await lock.owner()).to.equal(owner.address);
+        beforeEach(async () => {
+            const EIP20 = await ethers.getContractFactory("EIP20");
+            token = await EIP20.deploy('My Token', 'MT');
+            await token.deployed();
+            await token.transfer(contract.address, initialBalance);
+        });
+
+        it('should store the balance', async () => {
+            const balance = await token.balanceOf(contract.address);
+            assert.equal(balance.toNumber(), initialBalance);
+        });
+
+        //Note this submits, confirms, and executes the tx all at once because we only require 1 approval! 
+        //We send to token.address but the data contains the account (accounts[2]) at that address that we send to)
+        describe('executing an ERC20 transaction', function () {
+            beforeEach(async () => {
+                const data = token.interface.encodeFunctionData("transfer", [accounts[2], initialBalance]);
+                await contract.submitTransaction(token.address, 0, data);
+            });
+
+            it('should have removed the contract balance', async () => {
+                const balance = await token.balanceOf(contract.address);
+                assert.equal(balance.toNumber(), 0);
+            });
+
+            it('should have moved the balance to the destination', async () => {
+                const balance = await token.balanceOf(accounts[2]);
+                assert.equal(balance.toNumber(), initialBalance);
+            });
+        });
     });
 
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
+    describe('storing ether', function () {
+        const oneEther = ethers.utils.parseEther("1");
+        beforeEach(async () => {
+            await ethers.provider.getSigner(0).sendTransaction({ to: contract.address, value: oneEther });
+        });
 
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
+        it('should store the balance', async () => {
+            const balance = await ethers.provider.getBalance(contract.address);
+            assert.equal(balance.toString(), oneEther.toString());
+        });
+
+        describe('executing the ether transaction', function () {
+            let balanceBefore;
+
+            //When we submit the tx here we don't need to store anything in data becuase we are dealing with Eth
+            //We put the account (account[1]) as the destination. Not the "token contract" as we do when sending other tokens
+            beforeEach(async () => {
+                balanceBefore = await ethers.provider.getBalance(accounts[1]);
+                await contract.submitTransaction(accounts[1], oneEther, "0x");
+            });
+
+            it('should have removed the contract balance', async () => {
+                const balance = await ethers.provider.getBalance(contract.address);
+                assert.equal(balance, 0);
+            });
+
+            it('should have moved the balance to the destination', async () => {
+                const balance = await ethers.provider.getBalance(accounts[1]);
+                assert.equal(balance.sub(balanceBefore).toString(), oneEther.toString());
+            });
+        });
     });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
 });
+
